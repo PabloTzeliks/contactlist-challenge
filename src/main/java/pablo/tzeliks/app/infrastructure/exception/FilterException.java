67431bea636b8b351ef5787d02fd567d1ae14807
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-10 20:59:11 -0300

    feat: add Filter for phone number

---
+++ src/main/java/pablo/tzeliks/app/infrastructure/exception/FilterException.java
@@ -0,0 +1,7 @@
+package pablo.tzeliks.app.infrastructure.exception;
+
+public class FilterException extends InfrastructureException {
+    public FilterException(String message) {
+        super(message);
+    }
+}
--- src/main/java/pablo/tzeliks/app/infrastructure/filter/PhoneNumberFilterAdapter.java
+++ src/main/java/pablo/tzeliks/app/infrastructure/filter/PhoneNumberFilterAdapter.java
@@ -2,17 +2,36 @@ package pablo.tzeliks.app.infrastructure.filter;
 
 import org.springframework.stereotype.Component;
 import pablo.tzeliks.app.domain.ports.PhoneNumberFilterPort;
+import pablo.tzeliks.app.infrastructure.exception.FilterException;
 
 @Component
 public class PhoneNumberFilterAdapter implements PhoneNumberFilterPort {
 
-    @Override
-    public String minimize(String phoneNumber) {
-        return "";
+    public String minimize(String telefoneBruto) {
+        if (telefoneBruto == null) {
+            throw new FilterException("Telefone não pode ser nulo");
+        }
+
+        String apenasNumeros = telefoneBruto.replaceAll("\\D", "");
+
+        if (apenasNumeros.length() != 13) {
+            throw new FilterException(
+                    "Telefone inválido. Esperado 13 dígitos numéricos (País+DDD+9+Número). Recebido: " + apenasNumeros.length()
+            );
+        }
+
+        return apenasNumeros;
     }
 
-    @Override
-    public String maximize(String phoneNumber) {
-        return "";
+    public String maximize(String telefoneLimpo) {
+
+        if (telefoneLimpo == null || telefoneLimpo.length() != 13) {
+            return telefoneLimpo;
+        }
+
+        return telefoneLimpo.replaceAll(
+                "(\\d{2})(\\d{2})(\\d{1})(\\d{4})(\\d{4})",
+                "+$1 ($2) $3 $4-$5"
+        );
     }
 }
