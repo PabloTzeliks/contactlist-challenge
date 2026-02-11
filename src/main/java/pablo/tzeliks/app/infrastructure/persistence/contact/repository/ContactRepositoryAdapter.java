868be87427a05a789e717de48b5359eaded91685
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-11 15:37:05 -0300

    feat: add rule to the ContactRepository when saving

--- src/main/java/pablo/tzeliks/app/infrastructure/persistence/contact/repository/ContactRepositoryAdapter.java
+++ src/main/java/pablo/tzeliks/app/infrastructure/persistence/contact/repository/ContactRepositoryAdapter.java
@@ -3,6 +3,7 @@ package pablo.tzeliks.app.infrastructure.persistence.contact.repository;
 import org.springframework.stereotype.Repository;
 import pablo.tzeliks.app.domain.contact.model.Contact;
 import pablo.tzeliks.app.domain.contact.ports.ContactRepositoryPort;
+import pablo.tzeliks.app.infrastructure.persistence.contact.entity.ContactEntity;
 import pablo.tzeliks.app.infrastructure.persistence.contact.mapper.ContactMapper;
 
 import java.util.List;
@@ -23,7 +24,13 @@ public class ContactRepositoryAdapter implements ContactRepositoryPort {
     @Override
     public Contact save(Contact contact) {
 
-        return mapper.toDomain(mapper.toEntity(contact));
+        ContactEntity entity = mapper.toEntity(contact);
+
+        if (contact.getCreatedAt().equals(contact.getUpdatedAt())) {
+            entity.markAsNew();
+        }
+
+        return mapper.toDomain(entity);
     }
 
     @Override
