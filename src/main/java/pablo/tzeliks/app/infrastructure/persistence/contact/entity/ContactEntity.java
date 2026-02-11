e99ea4c682170b406cc8dba4c2ba4bcbf3e0c28f
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-11 01:49:05 -0300

    feat: add Contact Entity

--- src/main/java/pablo/tzeliks/app/infrastructure/persistence/contact/entity/ContactEntity.java
+++ src/main/java/pablo/tzeliks/app/infrastructure/persistence/contact/entity/ContactEntity.java
@@ -1,64 +1,72 @@
 package pablo.tzeliks.app.infrastructure.persistence.contact.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.domain.Persistable;
 
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Getter
 @Entity
-@Table(name = "contact")
-public class ContactEntity {
+@Table(name = "contacts",
+        uniqueConstraints = {
+            @UniqueConstraint(columnNames = {"owner_id", "phone_number"})
+        },
+        indexes = {
+            @Index(name = "idx_contact_owner", columnList = "owner_id")
+        })
+
+public class ContactEntity implements Persistable<UUID> {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name = "id", nullable = false)
+    @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
 
     @Column(name = "owner_id", nullable = false)
     private UUID ownerId;
 
-    @Column(name = "name", nullable = false, length = 122)
+    @Column(name = "name", nullable = false, length = 100)
     private String name;
 
-    @Column(name = "phone_number", nullable = false, unique = true, length = 13)
+    @Column(name = "phone_number", nullable = false, length = 13)
     private String phoneNumber;
 
-    @Column(name = "created_at", nullable = false)
-    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
     private Instant createdAt;
 
     @Column(name = "updated_at", nullable = false)
-    @CreationTimestamp
     private Instant updatedAt;
 
+    @Transient
+    private boolean isNew = false;
+
+    @Deprecated
     public ContactEntity() { }
 
-    public ContactEntity(UUID id, String name, String phoneNumber, Instant createdAt) {
+    public ContactEntity(UUID id, String name, String phoneNumber, Instant createdAt, Instant updatedAt) {
         this.id = id;
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
     }
 
+    public void markAsNew() {
+        this.isNew = true;
+    }
+
+    @Override
     public UUID getId() {
         return id;
     }
 
-    public String getName() {
-        return name;
+    @Override
+    public boolean isNew() {
+        return isNew;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }
+    public void setName(String name) { this.name = name; }
+    public void setUpdatedAt(Instant updatedAt) { this.updatedAt = updatedAt; }
 }
