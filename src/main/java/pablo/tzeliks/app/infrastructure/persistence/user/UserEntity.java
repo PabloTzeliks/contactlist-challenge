767be403badb83040d1f69e1bf9e26828d1c1307
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-11 01:40:44 -0300

    feat: add User Entity on Infra

--- src/main/java/pablo/tzeliks/app/infrastructure/persistence/user/UserEntity.java
+++ src/main/java/pablo/tzeliks/app/infrastructure/persistence/user/UserEntity.java
@@ -1,4 +1,59 @@
 package pablo.tzeliks.app.infrastructure.persistence.user;
 
-public class UserEntity {
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.domain.Persistable;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Entity
+@Table(name = "users")
+public class UserEntity implements Persistable<UUID> {
+
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Transient
+    private boolean isNew = false;
+
+    @Deprecated
+    public UserEntity() {
+    }
+
+    public UserEntity(UUID id, String username, String password, Instant createdAt, Instant updatedAt) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public void markAsNew() {
+        this.isNew = true;
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public boolean isNew() {
+        return isNew;
+    }
 }
