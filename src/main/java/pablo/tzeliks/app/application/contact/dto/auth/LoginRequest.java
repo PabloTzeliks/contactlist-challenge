56626b67191e2bb3fb82c7a69de82e5898b7b742
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-12 14:46:26 -0300

    feat: implement DTO for login request

---
+++ src/main/java/pablo/tzeliks/app/application/contact/dto/auth/LoginRequest.java
@@ -0,0 +1,8 @@
+package pablo.tzeliks.app.application.contact.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record LoginRequest(
+        @NotBlank(message = "Username é obrigatório") String username,
+        @NotBlank(message = "Password é obrigatório") String password
+) { }
---
+++ src/main/java/pablo/tzeliks/app/application/contact/dto/auth/LoginResponse.java
@@ -0,0 +1,4 @@
+package pablo.tzeliks.app.application.contact.dto.auth;
+
+public record LoginResponse() {
+}
