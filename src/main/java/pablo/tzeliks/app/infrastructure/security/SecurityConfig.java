2ee69ee49b6994b3072eb0049f90b06e64be9929
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-11 21:55:24 -0300

    feat: initialize the SecurityConfig

---
+++ src/main/java/pablo/tzeliks/app/infrastructure/security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package pablo.tzeliks.app.infrastructure.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+}
