1a514ce61b0173e7c0ba2faf736f8e2208fa2c37
PabloTzeliks <arq.pabloo@gmail.com>
2026-02-12 21:42:33 -0300

    feat: add Controller for Contact

--- src/main/java/pablo/tzeliks/app/infrastructure/web/controller/ContactController.java
+++ src/main/java/pablo/tzeliks/app/infrastructure/web/controller/ContactController.java
@@ -2,16 +2,18 @@ package pablo.tzeliks.app.infrastructure.web.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import pablo.tzeliks.app.application.contact.dto.ContactResponse;
 import pablo.tzeliks.app.application.contact.dto.CreateContactRequest;
 import pablo.tzeliks.app.application.contact.usecase.AddContactUseCase;
 
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/contact")
 public class ContactController {
 
     private AddContactUseCase addContact;
@@ -20,11 +22,15 @@ public class ContactController {
         this.addContact = addContact;
     }
 
-//    @PostMapping("/")
-//    public ResponseEntity<ContactResponse> add(@Valid @RequestBody CreateContactRequest request) {
-//
-//        ContactResponse response = addContact.execute(request);
-//
-//        return ResponseEntity.ok(response);
-//    }
+    @PostMapping("/")
+    public ResponseEntity<ContactResponse> add(@Valid @RequestBody CreateContactRequest request
+                                               @AuthenticationPrincipal User user,
+                                               UriComponentsBuilder uriBuilder) {
+
+        ContactResponse response = addContact.execute(request, user.getId);
+
+        var uri = uriBuilder.path("/contacts/{id}").buildAndExpand(response.id()).toUri();
+
+        return ResponseEntity.created(uri).body(response);
+    }
 }
